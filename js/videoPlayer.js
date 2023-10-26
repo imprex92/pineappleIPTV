3d090a352b6613923389f5ca3ecd5b8bd14c311d
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-10-27 00:29:32 +0200

    init

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,3 +1,20 @@
 const fs = require('fs');
 
-fs.readFile()
+const filePath = './playlists/tv_channels_fe918907_plus.m3u'
+
+fs.access(filePath, fs.constants.F_OK, (err) => {
+	if (err) {
+		console.error(`File with path ${filePath} does not exist!`);
+	}
+	else{
+		console.log("The file exists");
+		fs.readFile(filePath, (err, data) => {
+			if(err){
+				console.error(`Error reading the file: ${err}`);
+			}
+			else{
+				console.log(`File contents: ${data}`);
+			}
+		})
+	}
+})
