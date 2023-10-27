50bec848c3222f72c94b506d99bf5283f5696045
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-10-28 00:07:28 +0200

    Made sure url is there + cleanup

--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,48 +1,43 @@
-const filePath = './playlists/tv_channels_fe918907_plus.m3u'
-const channelList = document.getElementById('channelList') // ul element
-const messagesEl = document.getElementById('messages')
 
-fetch(filePath)
-.then((response) => {
-	if (response.status === 404) {
-		console.error('Playlist not found - Please check if the file exist.');
-	} else {
-		return response.text()
-	}
-})
-.then((m3uData) => {
-	m3uData && console.log('Playlist found');
-	const lines = m3uData.trim().split('\n')
-	const channelLines = lines.filter(line => line.startsWith('#EXTINF'))
-	let currentCountry = ''
+export async function fetchChannelList(){
+	const filePath = './playlists/tv_channels_fe918907_plus.m3u'
+	const channelList = document.getElementById('channelList') // ul element
+	const messagesEl = document.getElementById('messages')
 	
-	channelLines.forEach((line) => {
-		// let [, title] = /#EXTINF:.*,"(.*)"/.exec(line);
-
-		const channelName = line.match(/tvg-name="(.*?)"/)[1];
-
-		const countryMatch = line.match(/group-title="(.*?)"/)[1];
-		const country = countryMatch ? countryMatch : ''
-console.log(country);
-		// Add country header when changes (Not working too good)
-		if(country !== currentCountry){
-			const countryHeaderItem = document.createElement('li');
-			countryHeaderItem.textContent = country;
-			countryHeaderItem.className = 'country-header';
-			channelList.appendChild(countryHeaderItem);
-
-			currentCountry = country;
+	return await fetch(filePath)
+	.then((response) => {
+		if (response.status === 404) {
+			console.error('Playlist not found - Please check if the file exist.');
+		} else {
+			return response.text()
 		}
-
-		const listItem = document.createElement('li');
-		listItem.textContent = channelName;
-		listItem.className = 'channel-item'
-		channelList.appendChild(listItem);
 	})
-})
-.catch((error) => {
-	console.error(`Something went wrong while the wizard tried to summon your playlist - ${error}`);
-})
-
-
-//#EXTINF:-1 tvg-id="" tvg-name="US: FUBO SPORTS NETWORK HD" tvg-logo="https://github.com/Tapiosinn/tv-logos/blob/master/countries/united-states/fubo-sports-network-us.png?raw=true" group-title="USA",US: FUBO SPORTS NETWORK HD
+	.then((m3uData) => {
+		m3uData && console.log('Playlist found');
+		const lines = m3uData.trim().split('\n')
+		let channelInfo = []
+		
+		lines.forEach((line, index) => {
+			if (line.startsWith('#EXTINF')) {
+				const channelNameMatch = line.match(/tvg-name="(.*?)"/);
+				const channelName = channelNameMatch ? channelNameMatch[1] : '';
+	
+				const url = lines[index + 1];
+	
+				if (channelName && url) {
+					channelInfo.push({ name: channelName, url });
+				}
+			}
+		})
+		channelInfo.forEach((info) => {
+			const listItem = document.createElement('li');
+			listItem.textContent = info.name;
+			listItem.setAttribute('data-url', info.url)
+	
+			channelList.appendChild(listItem);
+		});
+	})
+	.catch((error) => {
+		console.error(`Something went wrong while the wizard tried to summon your playlist - ${error}`);
+	})
+}
