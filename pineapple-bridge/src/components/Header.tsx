d43a264da2ef01b2bd24662fee1b4dce7262bdf9
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-11-08 21:23:30 +0100

    New header + nav

--- pineapple-bridge/src/components/Header.tsx
+++ pineapple-bridge/src/components/Header.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import pineApple from '../assets/PPG-Happy-Pineapple-SVG.svg'
+import styles from '../styles/header.module.css'
 import Nav from './Nav';
 
 export interface IHeaderProps {
@@ -6,9 +8,9 @@ export interface IHeaderProps {
 
 export default function Header (props: IHeaderProps) {
   return (
-	<header id="header">
-		<img src={pineApple} width="32" alt="Logo" />
-		<h1>Pineapple IPTV</h1>
+	<header id="header" className={styles.header}>
+		<Link to={'/'}><img src={pineApple} width="32" alt="Logo" /></Link>
+		<h1 className={styles.title}>Pineapple IPTV</h1>
 		<Nav />
 	</header>
   );
--- pineapple-bridge/src/components/Nav.tsx
+++ pineapple-bridge/src/components/Nav.tsx
@@ -1,14 +1,15 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styles from '../styles/header.module.css'
 
 export interface INavProps {
 }
 
 export default function Nav (props: INavProps) {
   return (
-	<nav>
-		<ul className="navListWrapper">
-			<li className="navItem"><Link to={'/'}>Home</Link></li>
-			<li className="navItem"><Link to={'/about'}>About this page</Link></li>
+	<nav className={styles.navContainer}>
+		<ul className={styles.navList}>
+			<li className={styles.navItem}><NavLink to={'/'} className={({isActive}) => (isActive ? `${styles.nav} ${styles.nav_active}` : styles.nav)}>Home</NavLink></li>
+			<li className={styles.navItem}><NavLink to={'/about'} className={({isActive}) => (isActive ? `${styles.nav} ${styles.nav_active}` : styles.nav)}>About this page</NavLink></li>
 		</ul>
 	</nav>
   );
