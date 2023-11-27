270f645db317c5120e96795eeae45f42b761955b
Daniel Bréelle Johannesson <56023250+imprex92@users.noreply.github.com>
2023-11-27 23:42:26 +0100

    Surrounded text in span

--- pineapple-bridge/src/Pages/About.tsx
+++ pineapple-bridge/src/Pages/About.tsx
@@ -1,46 +1,47 @@
-import { t } from 'i18next';
+import { useTranslation } from 'react-i18next';
 import styles from '../styles/aboutus.module.css'
 
 export interface IAboutProps {
 }
 
 export default function About () {
+	const { t } = useTranslation()
   return (
 	<div className={styles.wrapper}>
-  <h1 className={styles.heading}>{t('welcome.heading')}</h1>
+		<h1 className={styles.heading}>{t('welcome.heading')}</h1>
 
-  <h2>{t('about.heading')}</h2>
+		<h2>{t('about.heading')}</h2>
 
-  <p className={styles.paragraf}><span className={styles.highlight}>{t('about.whatIsIPTV')}</span><br/>
-    {t('about.welcomeDescription')}</p>
+		<p className={styles.paragraf}><span className={styles.highlight}>{t('about.whatIsIPTV')}</span><br/>
+			{t('about.welcomeDescription')}</p>
 
-  <p className={styles.paragraf}><span className={styles.highlight}>{t('about.whatMakesIPTVSpecial')}</span><br/>
-    {t('about.specialDescription')}</p>
+		<p className={styles.paragraf}><span className={styles.highlight}>{t('about.whatMakesIPTVSpecial')}</span><br/>
+			{t('about.specialDescription')}</p>
 
-  <p className={styles.paragraf}><span className={styles.highlight}>{t('about.howItWorks')}</span></p>
+		<p className={styles.paragraf}><span className={styles.highlight}>{t('about.howItWorks')}</span></p>
 
-  <ul className={styles.list}>
-    <li className={styles.listItem}><span className={styles.highlight}>{t('about.uploadPlaylist')}</span> {t('about.chooseLanguage')}</li>
-    <li className={styles.listItem}><span className={styles.highlight}>{t('about.setEmailCode')}</span> {t('about.securityNote')}</li>
-    <li className={styles.listItem}><span className={styles.highlight}>{t('about.downloadToLGSmartTV')}</span> {t('about.enjoyChannels')}</li>
-  </ul>
+		<ul className={styles.list}>
+			<li className={styles.listItem}><span className={styles.highlight}>{t('about.uploadPlaylist')}</span> <span>{t('about.chooseLanguage')}</span></li>
+			<li className={styles.listItem}><span className={styles.highlight}>{t('about.setEmailCode')}</span> <span>{t('about.securityNote')}</span></li>
+			<li className={styles.listItem}><span className={styles.highlight}>{t('about.downloadToLGSmartTV')}</span> <span>{t('about.enjoyChannels')}</span></li>
+		</ul>
 
-  <p className={styles.paragraf}><span className={styles.highlight}>{t('about.whyPineappleIPTV')}</span></p>
+		<p className={styles.paragraf}><span className={styles.highlight}>{t('about.whyPineappleIPTV')}</span></p>
 
-  <ul className={styles.list}>
-    <li className={styles.listItem}><span className={styles.highlight}>{t('about.userFriendly')}</span> {t('about.userFriendlyDescription')}</li>
-    <li className={styles.listItem}><span className={styles.highlight}>{t('about.secureStorage')}</span> {t('about.secureStorageDescription')}</li>
-    <li className={styles.listItem}><span className={styles.highlight}>{t('about.multilingualSupport')}</span> {t('about.multilingualSupportDescription')}</li>
-  </ul>
+		<ul className={styles.list}>
+			<li className={styles.listItem}><span className={styles.highlight}>{t('about.userFriendly')}</span> <span>{t('about.userFriendlyDescription')}</span></li>
+			<li className={styles.listItem}><span className={styles.highlight}>{t('about.secureStorage')}</span> <span>{t('about.secureStorageDescription')}</span></li>
+			<li className={styles.listItem}><span className={styles.highlight}>{t('about.multilingualSupport')}</span> <span>{t('about.multilingualSupportDescription')}</span></li>
+		</ul>
 
-  <p className={styles.paragraf}><span className={styles.highlight}>{t('about.joinCommunity')}</span><br/>
-    {t('about.soloEndeavor')}</p>
+		<p className={styles.paragraf}><span className={styles.highlight}>{t('about.joinCommunity')}</span><br/>
+			{t('about.soloEndeavor')}</p>
 
-  <p className={styles.paragraf}><span className={styles.highlight}>{t('about.getInTouch')}</span><br/>
-    {t('about.contactInfo')}</p>
+		<p className={styles.paragraf}><span className={styles.highlight}>{t('about.getInTouch')}</span><br/>
+			{t('about.contactInfo')}</p>
 
-  <p className={styles.paragraf}>{t('about.happyWatching')} <span className={styles.highlight}>🍍✨</span></p>
+		<p className={styles.paragraf}>{t('about.happyWatching')} <span className={styles.highlight}>🍍✨</span></p>
 
-</div>
+	</div>
   );
 }
